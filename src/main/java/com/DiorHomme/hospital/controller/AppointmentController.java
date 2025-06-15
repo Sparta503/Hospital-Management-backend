b9d88d2df9a9d32f3865fe63ecee64b4de14ae95
DiorHomme <chomunodisaa68@gmail.com>
2025-06-15 11:44:37 +0200

    Update AppointmentController.java

--- src/main/java/com/DiorHomme/hospital/controller/AppointmentController.java
+++ src/main/java/com/DiorHomme/hospital/controller/AppointmentController.java
@@ -1,5 +1,30 @@
 package com.DiorHomme.hospital.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import com.DiorHomme.hospital.service.Appointment;
+import com.DiorHomme.hospital.model.AppointmentModel;
+import java.util.List;
+
+@RestController
+@RequestMapping("/appointments")
 public class AppointmentController {
-    
+
+    @Autowired
+    private Appointment appointmentService;
+
+    @GetMapping
+    public List<AppointmentModel> getAllAppointments() {
+        return appointmentService.getAllAppointments();
+    }
+
+    @PostMapping
+    public AppointmentModel createAppointment(@RequestBody AppointmentModel appointment) {
+        return appointmentService.createAppointment(appointment);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteAppointment(@PathVariable Long id) {
+        appointmentService.deleteAppointment(id);
+    }
 }
