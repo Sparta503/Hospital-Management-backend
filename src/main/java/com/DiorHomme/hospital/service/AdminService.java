92df3f35551f6835049b1458b6ff904d2ea4ca3a
DiorHomme <chomunodisaa68@gmail.com>
2025-06-15 11:44:00 +0200

    Update AdminService.java

--- src/main/java/com/DiorHomme/hospital/service/AdminService.java
+++ src/main/java/com/DiorHomme/hospital/service/AdminService.java
@@ -1,5 +1,32 @@
 package com.DiorHomme.hospital.service;
 
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.DiorHomme.hospital.model.AdminUser;
+import com.DiorHomme.hospital.repository.AdminUserRepository;
+
+@Service
 public class AdminService {
+
+    @Autowired
+    private AdminUserRepository adminUserRepository;
+
+    public List<AdminUser> getAllAdminUsers() {
+        return adminUserRepository.findAll();
+    }
+
+    public AdminUser createAdminUser(AdminUser adminUser) {
+        return adminUserRepository.save(adminUser);
+    }
+
+    public boolean existsById(Long id) {
+        return adminUserRepository.existsById(id);
+    }
+
+    public void deleteAdminUser(Long id) {
+        adminUserRepository.deleteById(id);
+    }
     
 }
