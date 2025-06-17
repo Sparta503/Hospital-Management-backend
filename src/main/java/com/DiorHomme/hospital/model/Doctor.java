fc4ba468b5da9eebaf7777acf52bf1ae014d7568
DiorHomme <chomunodisaa68@gmail.com>
2025-06-17 12:36:06 +0200

    Update Doctor.java

--- src/main/java/com/DiorHomme/hospital/model/Doctor.java
+++ src/main/java/com/DiorHomme/hospital/model/Doctor.java
@@ -1,5 +1,85 @@
 package com.DiorHomme.hospital.model;
 
+import java.util.List;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "doctors")
 public class Doctor {
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;  
+
+    @Column
+    private String name;
+
+    @Column
+    private String email;
+
+    @Column
+    private String specialization;
+
+    @Column
+    private String phoneNumber;
+
+    @OneToMany(mappedBy = "doctor", fetch = FetchType.LAZY)
+    private List<AppointmentModel> appointments;
+
+    public Doctor() {}
+
+    public Doctor(Long id, String name, String email, String specialization, String phoneNumber) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.specialization = specialization;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSpecialization() {
+        return specialization;
+    }
+
+    public void setSpecialization(String specialization) {
+        this.specialization = specialization;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public List<AppointmentModel> getAppointments() {
+        return appointments;
+    }
+
+    public void setAppointments(List<AppointmentModel> appointments) {
+        this.appointments = appointments;
+    }
 }
