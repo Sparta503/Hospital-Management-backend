ce3b4aa46264853ff21e7e45ff823f30c44d4348
DiorHomme <chomunodisaa68@gmail.com>
2025-06-19 12:01:26 +0200

    Create NotificationService.java

---
+++ src/main/java/com/DiorHomme/hospital/service/NotificationService.java
@@ -0,0 +1,42 @@
+package com.DiorHomme.hospital.service;
+
+import com.DiorHomme.hospital.model.Notification;
+import com.DiorHomme.hospital.repository.NotificationRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class NotificationService {
+
+    @Autowired
+    private NotificationRepository notificationRepository;
+
+    public List<Notification> getAllNotifications() {
+        return notificationRepository.findAll();
+    }
+
+    public Notification createNotification(Notification notification) {
+        return notificationRepository.save(notification);
+    }
+
+    public Optional<Notification> getNotificationById(Long id) {
+        return notificationRepository.findById(id);
+    }
+
+    public void deleteNotification(Long id) {
+        notificationRepository.deleteById(id);
+    }
+
+    public Notification markAsRead(Long id) {
+        Optional<Notification> optional = notificationRepository.findById(id);
+        if (optional.isPresent()) {
+            Notification notification = optional.get();
+            notification.setRead(true);
+            return notificationRepository.save(notification);
+        }
+        return null;
+    }
+}
