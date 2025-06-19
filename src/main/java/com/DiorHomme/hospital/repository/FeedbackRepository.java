713562554192206fd26e4183a7c4d8bfa6433cee
DiorHomme <chomunodisaa68@gmail.com>
2025-06-19 12:01:59 +0200

    Update FeedbackRepository.java

--- src/main/java/com/DiorHomme/hospital/repository/FeedbackRepository.java
+++ src/main/java/com/DiorHomme/hospital/repository/FeedbackRepository.java
@@ -1,5 +1,9 @@
 package com.DiorHomme.hospital.repository;
 
-public class FeedbackRepository {
-    
+import com.DiorHomme.hospital.model.Feedback;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface FeedbackRepository extends JpaRepository<Feedback, Long> {
 }
