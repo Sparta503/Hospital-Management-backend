cf960df65af2f20ad6b9aecbd1ea930b1a407113
DiorHomme <chomunodisaa68@gmail.com>
2025-06-19 12:01:53 +0200

    Update NotificationRepository.java

--- src/main/java/com/DiorHomme/hospital/repository/NotificationRepository.java
+++ src/main/java/com/DiorHomme/hospital/repository/NotificationRepository.java
@@ -1,5 +1,9 @@
 package com.DiorHomme.hospital.repository;
 
-public class NotificationRepository {
-    
+import com.DiorHomme.hospital.model.Notification;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface NotificationRepository extends JpaRepository<Notification, Long> {
 }
