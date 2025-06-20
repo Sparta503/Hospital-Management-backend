127f9e050deb143c737112c3f4c3468f4f643312
DiorHomme <chomunodisaa68@gmail.com>
2025-06-20 16:42:41 +0200

    Update PatientService.java

--- src/main/java/com/DiorHomme/hospital/service/PatientService.java
+++ src/main/java/com/DiorHomme/hospital/service/PatientService.java
@@ -1,5 +1,12 @@
 package com.DiorHomme.hospital.service;
 
-public class PatientService {
-    
+import com.DiorHomme.hospital.model.Patient;
+import java.util.List;
+
+public interface PatientService {
+    List<Patient> getAllPatients();
+    Patient getPatientById(Long id);
+    Patient createPatient(Patient patient);
+    Patient updatePatient(Long id, Patient patient);
+    boolean deletePatient(Long id);
 }
