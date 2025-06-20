fa70b8c5f33b9f2fc61b028382456619856e6ee0
DiorHomme <chomunodisaa68@gmail.com>
2025-06-20 16:42:30 +0200

    Create PatientRepository.java

---
+++ src/main/java/com/DiorHomme/hospital/repository/PatientRepository.java
@@ -0,0 +1,9 @@
+package com.DiorHomme.hospital.repository;
+
+import com.DiorHomme.hospital.model.Patient;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PatientRepository extends JpaRepository<Patient, Long> {
+}
